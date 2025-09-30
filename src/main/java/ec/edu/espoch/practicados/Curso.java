efc5032d35e9375af195400878ff711fb88e2b06
saezfernanda10 <ferchasaez2005@gmail.com>
2025-09-30 12:08:17 -0500

    Creacion de las clases estudiantes y curso

---
+++ src/main/java/ec/edu/espoch/practicados/Curso.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.practicados;
+
+public class Curso {
+
+    //atributos
+    public String nombreCurso;
+    public String codigo;
+    public Estudiante[] estudiantesInstritos;
+
+    //metodos
+    public void agregarEstudiante()
+    {
+
+    }
+
+    public void eliminarEstudiante()
+    {
+
+    }
+
+    public void mostrarTodosLosEstudiantes()
+    {
+
+    }
+}
---
+++ src/main/java/ec/edu/espoch/practicados/Estudiante.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.practicados;
+
+public class Estudiante {
+
+    //atributos
+    public String nombre;
+    public String identificacion;
+    public float calificacionPromedio;
+
+    //metodos
+    public void inscrito()
+
+    {
+
+    }
+
+    public void retirarse()
+
+    {
+
+    }
+
+    public void informacionEstudiante()
+
+    {
+        
+    }
+    
+}
